8e2fd6f797cedf27aa3afd9efeca206819b666ae
yodering <david.yoder@live.com>
2023-11-11 15:30:25 -0500

    added node system

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,96 @@
-
-/* javascript */
+document.addEventListener('DOMContentLoaded', function () {
+    const svg = document.getElementById('graph');
+    let selectedNode = null;
+    let offset = null;
+    let lineIdCounter = 0; // A counter to give unique ids to the lines
+  
+    // Function to create a line
+    function createLine(x1, y1, x2, y2) {
+      const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+      line.setAttribute("x1", x1);
+      line.setAttribute("y1", y1);
+      line.setAttribute("x2", x2);
+      line.setAttribute("y2", y2);
+      line.classList.add("line");
+      line.setAttribute('id', 'line-' + lineIdCounter); // Assign a unique id to the line
+      lineIdCounter++; // Increment the counter
+      svg.insertBefore(line, svg.firstChild); // Ensure the line is under the nodes
+      return line;
+    }
+  
+    function createNode(x, y, isCentral) {
+      const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+      circle.setAttribute("cx", x);
+      circle.setAttribute("cy", y);
+      circle.setAttribute("r", isCentral ? 100 : 50);
+      circle.classList.add("node");
+      if (!isCentral) {
+        // Non-central nodes get a data attribute linking to their line
+        const line = createLine(x, y, window.innerWidth / 2, window.innerHeight / 2);
+        circle.setAttribute('data-line', line.id);
+      } else {
+        circle.setAttribute('id', 'central-node');
+      }
+      svg.appendChild(circle);
+      return circle;
+    }
+  
+    // Add a central node
+    const centralNode = createNode(window.innerWidth / 2, window.innerHeight / 2, true);
+  
+    svg.addEventListener('dblclick', function (event) {
+      const rect = svg.getBoundingClientRect();
+      createNode(event.clientX - rect.left, event.clientY - rect.top, false);
+    });
+  
+    function startDrag(event) {
+      if (event.target.classList.contains('node') && event.target.id !== 'central-node') {
+        selectedNode = event.target;
+        offset = getMousePosition(event);
+        offset.x -= parseFloat(selectedNode.getAttribute('cx'));
+        offset.y -= parseFloat(selectedNode.getAttribute('cy'));
+        svg.addEventListener('mousemove', drag);
+      }
+    }
+  
+    function drag(event) {
+        if (selectedNode) {
+          const coord = getMousePosition(event);
+          const dx = coord.x - offset.x;
+          const dy = coord.y - offset.y;
+          selectedNode.setAttribute('cx', dx);
+          selectedNode.setAttribute('cy', dy);
+    
+          // Retrieve and update the corresponding line
+          const lineId = selectedNode.getAttribute('data-line');
+          if (lineId) {
+            const line = document.getElementById(lineId);
+            line.setAttribute('x1', dx);
+            line.setAttribute('y1', dy);
+          }
+        }
+      }
+    
+  
+    function endDrag(event) {
+      selectedNode = null;
+      svg.removeEventListener('mousemove', drag);
+    }
+  
+    function getMousePosition(event) {
+      const CTM = svg.getScreenCTM();
+      return {
+        x: (event.clientX - CTM.e) / CTM.a,
+        y: (event.clientY - CTM.f) / CTM.d
+      };
+    }
+  
+    // Event listeners for drag functionality
+    svg.addEventListener('mousedown', startDrag);
+    svg.addEventListener('mouseup', endDrag);
+    svg.addEventListener('mouseleave', endDrag);
+  
+    // Ensure the central node is on top by re-appending it to the SVG
+    svg.appendChild(centralNode);
+  });
+  
