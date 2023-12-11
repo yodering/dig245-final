3f0cc3d5dde5d341294c8e083cbe8a7ce4f66cb2
yodering <david.yoder@live.com>
2023-12-11 16:46:03 -0500

    added save playlist function

--- assets/js/main.js
+++ assets/js/main.js
@@ -340,8 +340,6 @@ function spotifyLogin() {
   // redirect current window to Spotify login
   window.location.href = authUrl;
 
-  // logging to console
-  console.log("Redirecting to Spotify login with URL:", authUrl);
 }
 
 
@@ -481,6 +479,74 @@ async function displayImage(token, artistId, artistName) {
     }
   }
 }
+
+
+async function savePlaylistToSpotify() {
+  // check for user access token
+  if (!userAccessToken) {
+    alert('Please log in to Spotify first.');
+    return;
+  }
+
+  // get the user's Spotify ID
+  const userId = await fetchSpotifyUserId();
+
+  // get the playlist name from the input field
+  const playlistName = document.getElementById('playlistNameInput').value || 'My New Playlist';
+
+  // create playlist
+  const playlistId = await createSpotifyPlaylist(userId, playlistName);
+
+  // collect track URIs
+  const trackUris = Object.values(artistSongsMap).flat().map(song => song.uri);
+
+  // add tracks to the new playlist
+  await addTracksToPlaylist(playlistId, trackUris);
+
+  alert(`Playlist '${playlistName}' saved to your Spotify account!`);
+}
+
+async function fetchSpotifyUserId() {
+  const response = await fetch('https://api.spotify.com/v1/me', {
+    headers: {
+      'Authorization': `Bearer ${userAccessToken}`
+    }
+  });
+  const data = await response.json();
+  return data.id;
+}
+
+async function createSpotifyPlaylist(userId, playlistName) {
+  const response = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${userAccessToken}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: playlistName,
+      description: 'Created from my app',
+      public: false
+    })
+  });
+  const data = await response.json();
+  return data.id;
+}
+
+async function addTracksToPlaylist(playlistId, trackUris) {
+  await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${userAccessToken}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      uris: trackUris
+    })
+  });
+}
+
+document.getElementById('savePlaylistButton').addEventListener('click', savePlaylistToSpotify);
 
 
 
